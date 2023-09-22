0372b7dae0d760f0c588fe2bf36e55a5b1fb175a
HaxelCR <123345988+HaxelCR@users.noreply.github.com>
2023-09-21 23:59:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ bot.on(["/15"], (msg) => {
     bot.sendMessage(msg.chat.id, `Hola,fui creado por tu flako dienton, y quiere recordarte que te ama demasiado y eres una mujer muy especial para el, por si acaso si se te olvida ❤. Pronto habra mas funciones.`)
 })
 
+bot.on(["/triste"], (msg) => {
+    bot.sendMessage(msg.chat.id, `Recuerda que tu puedes con todo, eres mi mujer, y mi mujer no se deja vencer por nada`)
+
+})
+
 bot.start();
